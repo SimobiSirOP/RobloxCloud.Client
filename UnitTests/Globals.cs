fdc3048c37e3aa0a99516ec7b7236e45ef1558a7
SimobiSirOP <simobisirop@gmail.com>
2026-02-08 20:33:14 +0300

    Workflow change

--- UnitTests/Globals.cs
+++ UnitTests/Globals.cs
@@ -15,7 +15,7 @@ public class GlobalInit
     public void RunBeforeAnyTests()
     {
         var botToken = System.Environment.GetEnvironmentVariable("ROBLOX_API_TOKEN");
-        Globals.TestUniverseId = long.Parse(System.Environment.GetEnvironmentVariable("UNIVERSE_ID")!);
+        Globals.TestUniverseId = long.Parse(System.Environment.GetEnvironmentVariable("ROBLOX_UNIVERSE_ID")!);
         Globals.Client = new RobloxApiClient(new RobloxApiClientSettings(botToken, TimeSpan.FromSeconds(20), 7));
     }
 }
--- UnitTests/UnitTest1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UnitTests;
-
-public class Tests
-{
-    // TODO Find a ways to implement tests
-}
